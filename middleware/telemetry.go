11a320f6bebaa051285449aaee6c301acca4fe7a
Cesar Galvis <cgalvisl@cgalvisleon.com>
2023-10-31 09:30:41 -0500

    Backup

--- middleware/telemetry.go
+++ middleware/telemetry.go
@@ -10,20 +10,38 @@ import (
 	"github.com/cgalvisleon/elvis/envar"
 	"github.com/cgalvisleon/elvis/event"
 	. "github.com/cgalvisleon/elvis/json"
+	. "github.com/cgalvisleon/elvis/utilities"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
+type Request struct {
+	Tag     string
+	Day     int
+	Hour    int
+	Minute  int
+	Seccond int
+	Limit   int
+}
+
+func CallRequests(tag string) Request {
+	return Request{
+		Tag:     tag,
+		Day:     cache.More(fmt.Sprintf(`%s-%d`, tag, time.Now().Unix()/86400), 86400),
+		Hour:    cache.More(fmt.Sprintf(`%s-%d`, tag, time.Now().Unix()/3600), 3600),
+		Minute:  cache.More(fmt.Sprintf(`%s-%d`, tag, time.Now().Unix()/60), 60),
+		Seccond: cache.More(fmt.Sprintf(`%s-%d`, tag, time.Now().Unix()/2), 2),
+		Limit:   envar.EnvarInt(1000, "REQUESTS_LIMIT"),
+	}
+}
+
 func Telemetry(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
+		_id := NewId()
 
 		if rctx := chi.RouteContext(ctx); rctx != nil {
-			endPoint := "/"
-			n := len(rctx.RoutePatterns)
-			if n > 0 {
-				endPoint = fmt.Sprintf(`%s`, rctx.RoutePatterns[n-1])
-			}
+			endPoint := r.URL.Path
 			t1 := time.Now()
 			hostName, _ := os.Hostname()
 			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
@@ -32,34 +50,42 @@ func Telemetry(next http.Handler) http.Handler {
 				headers[key] = val
 			}
 
-			requests_day := cache.More(fmt.Sprintf(`%d`, time.Now().Unix()/86400), 86400)
-			requests_hour := cache.More(fmt.Sprintf(`%d`, time.Now().Unix()/3600), 3600)
-			requests_minute := cache.More(fmt.Sprintf(`%d`, time.Now().Unix()/60), 60)
-			requests_second := cache.More(fmt.Sprintf(`%d`, time.Now().Unix()/2), 2)
-			limit := envar.EnvarInt(1000, "REQUESTS_LIMIT")
+			requests_host := CallRequests(hostName)
+			requests_endpoint := CallRequests(endPoint)
 
 			summary := Json{
-				"datetime":        t1,
-				"host_name":       hostName,
-				"method":          rctx.RouteMethod,
-				"endpoint":        endPoint,
-				"status":          ww.Status(),
-				"bytes_written":   ww.BytesWritten(),
-				"header":          headers,
-				"since":           time.Since(t1),
-				"requests_day":    requests_day,
-				"requests_hour":   requests_hour,
-				"requests_minute": requests_minute,
-				"requests_second": requests_second,
-				"limit":           limit,
+				"_id":           _id,
+				"datetime":      t1,
+				"host_name":     hostName,
+				"method":        rctx.RouteMethod,
+				"endpoint":      endPoint,
+				"status":        ww.Status(),
+				"bytes_written": ww.BytesWritten(),
+				"header":        headers,
+				"since":         time.Since(t1),
+				"request_host": Json{
+					"day":    requests_host.Day,
+					"hour":   requests_host.Hour,
+					"minute": requests_host.Minute,
+					"second": requests_host.Seccond,
+					"limit":  requests_host.Limit,
+				},
+				"requests_endpoint": Json{
+					"day":    requests_endpoint.Day,
+					"hour":   requests_endpoint.Hour,
+					"minute": requests_endpoint.Minute,
+					"second": requests_endpoint.Seccond,
+					"limit":  requests_endpoint.Limit,
+				},
 			}
 			event.EventPublish("telemetry", summary)
 
-			if requests_second >= limit {
+			if requests_host.Seccond >= requests_host.Limit {
 				event.EventPublish("requests/overflow", summary)
 			}
 		}
 
+		w.Header().Set("_id", _id)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
