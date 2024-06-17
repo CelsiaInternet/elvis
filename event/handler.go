8b9cda4041d7ca2e93cccf63f53c23815074498e
Cesar Galvis Leon <cgalvisleon@gmail.com>
2024-06-17 10:51:07 -0500

    Update

--- event/handler.go
+++ event/handler.go
@@ -1,14 +1,18 @@
 package event
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/cgalvisleon/elvis/cache"
 	"github.com/cgalvisleon/elvis/et"
+	"github.com/cgalvisleon/elvis/logs"
+	"github.com/cgalvisleon/elvis/response"
 	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
 )
 
+// Basic function to publish a message to a channel
 func Publish(clientId, channel string, data map[string]interface{}) error {
 	if conn == nil {
 		return nil
@@ -35,46 +39,7 @@ func Publish(clientId, channel string, data map[string]interface{}) error {
 	return conn.conn.Publish(msg.Type(), dt)
 }
 
-func Event(event string, data interface{}) {
-	go Publish("event", "event/publish", et.Json{
-		"event": event,
-		"data":  data,
-	})
-}
-
-func Work(work, work_id string, data interface{}) {
-	go Publish("event", work, et.Json{
-		"work":    work,
-		"work_id": work_id,
-		"data":    data,
-	})
-}
-
-func Working(worker, work_id string) {
-	go Publish("event", "event/working", et.Json{
-		"worker":  worker,
-		"work_id": work_id,
-	})
-}
-
-func Done(work_id, event string) {
-	go Publish("event", "event/done", et.Json{
-		"work_id": work_id,
-		"event":   event,
-	})
-}
-
-func Rejected(work_id, event string) {
-	go Publish("event", "event/rejected", et.Json{
-		"work_id": work_id,
-		"event":   event,
-	})
-}
-
-func Action(action string, data map[string]interface{}) {
-	go Publish("action", action, data)
-}
-
+// Basic function to subscribe to a channel
 func Subscribe(channel string, f func(CreatedEvenMessage)) (err error) {
 	if conn == nil {
 		return
@@ -91,6 +56,7 @@ func Subscribe(channel string, f func(CreatedEvenMessage)) (err error) {
 	return
 }
 
+// Basic function to subscrite kind stack to a channel
 func Stack(channel string, f func(CreatedEvenMessage)) (err error) {
 	if conn == nil {
 		return
@@ -115,25 +81,81 @@ func Stack(channel string, f func(CreatedEvenMessage)) (err error) {
 	return
 }
 
-func WsBroadcast(message interface{}, ignoreId string) {
-	go Publish("ws", "ws/broadcast", et.Json{
-		"message":   message,
-		"ignore_id": ignoreId,
+/**
+* Event Worker
+**/
+
+// Publish event
+func Worker(event string, data et.Json) {
+	// Publish event
+	go Publish("service_event", event, data)
+
+	// Publish log a eente
+	go Publish("service_event", "event/publish", et.Json{
+		"event": event,
+		"data":  data,
 	})
+
+	logs.Log("Service event", "event:", event, " - data:", data.ToString())
 }
 
-func WsPublish(channel string, message interface{}, ignoreId string) {
-	go Publish("ws", "ws/publish", et.Json{
-		"channel":   channel,
-		"message":   message,
-		"ignore_id": ignoreId,
+// Publish event asigned to a worker
+func Work(worker, work_id string, data et.Json) {
+	go Publish("service_event", "event/work", et.Json{
+		"work":    worker,
+		"work_id": work_id,
+		"data":    data,
 	})
+
+	logs.Log("Service event", "worker:", worker, " - data:", data.ToString())
 }
 
-func WsSendMessage(clientId, channel string, message interface{}) {
-	go Publish("ws", "ws/send_message", et.Json{
-		"client_id": clientId,
-		"channel":   channel,
-		"message":   message,
+// Publish event begin work
+func Working(worker, work_id string) {
+	go Publish("service_event", "event/work/begin", et.Json{
+		"worker":  worker,
+		"work_id": work_id,
 	})
+
+	logs.Log("Service event", "worker:", worker, " - worker_id:", work_id)
+}
+
+// Done work
+func Done(work_id, event string) {
+	go Publish("service_event", "event/work/done", et.Json{
+		"work_id": work_id,
+		"event":   event,
+	})
+
+	logs.Log("Service event", "event:", event, " - worker_id:", work_id)
+}
+
+// Rejected work
+func Rejected(work_id, event string) {
+	go Publish("service_event", "event/work/rejected", et.Json{
+		"work_id": work_id,
+		"event":   event,
+	})
+
+	logs.Log("Service event", "event:", event, " - worker_id:", work_id)
+}
+
+func Action(action string, data map[string]interface{}) {
+	go Publish("service_action", action, data)
+}
+
+// http
+func Connect(w http.ResponseWriter, r *http.Request) {
+	body, _ := response.GetBody(r)
+	event := body.Str("event")
+	data := body.Json("data")
+
+	Worker(event, data)
+
+	response.JSON(w, r, http.StatusOK, et.Json{
+		"status":  "ok",
+		"event":   event,
+		"message": "Event published",
+	})
+
 }
