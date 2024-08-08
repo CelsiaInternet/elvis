9646d942810ba486c09035e9fb9a2483052a9fd7
jdmunozg <72575589+jdmunozg@users.noreply.github.com>
2024-08-08 13:48:50 -0500

    Feat(Connect with mongo db )

--- logs/connect.go
+++ logs/connect.go
@@ -1,1 +1,41 @@
 package logs
+
+import (
+	"context"
+
+	"github.com/cgalvisleon/elvis/envar"
+	"github.com/cgalvisleon/elvis/msg"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func connect() (*Conn, error) {
+	host := envar.EnvarStr("", "MONGO_HOST")
+	password := envar.EnvarStr("", "MONGO_PASSWORD")
+	dbname := envar.EnvarInt(0, "MONGO_DB")
+
+	if host == "" {
+		return nil, Alertf(msg.ERR_ENV_REQUIRED, "MONGO_HOST")
+	}
+
+	if password == "" {
+		return nil, Alertf(msg.ERR_ENV_REQUIRED, "MONGO_PASSWORD")
+	}
+
+	client := options.Client().ApplyURI(host).SetAuth(options.Credential{Password: password}).SetDirect(true)
+	clients, err := mongo.Connect(context.Background(), client)
+	if err != nil {
+		Errorf("Mongo", "Error connecting to MongoDB: %v", err)
+	}
+	err = clients.Ping(context.TODO(), nil)
+	if err != nil {
+		Errorf("Mongo", "Error connecting to MongoDB: %v", err)
+	}
+
+	return &Conn{
+		ctx:    context.Background(),
+		host:   host,
+		dbname: dbname,
+		db:     clients,
+	}, nil
+}
--- logs/logs.go
+++ logs/logs.go
@@ -1,1 +1,38 @@
 package logs
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var conn *Conn
+
+type Conn struct {
+	ctx    context.Context
+	host   string
+	dbname int
+	db     *mongo.Client
+}
+
+func Load() (*Conn, error) {
+	if conn != nil {
+		return conn, nil
+	}
+
+	var err error
+	conn, err = connect()
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func Close() error {
+	if conn.db == nil {
+		return nil
+	}
+
+	return conn.db.Disconnect(conn.ctx)
+}
