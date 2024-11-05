900108d4cbc167ffda5a6873f28e1085ec4bf0bd
Cesar Galvis Leon <cgalvisleon@gmail.com>
2024-11-05 05:38:06 -0500

    Backup

--- jdb/records.go
+++ jdb/records.go
@@ -25,6 +25,7 @@ func defineRecords(db *DB) error {
     TABLE_SCHEMA VARCHAR(80) DEFAULT '',
     TABLE_NAME VARCHAR(80) DEFAULT '',
 		OPTION VARCHAR(80) DEFAULT '',
+		SYNC BOOLEAN DEFAULT FALSE,
     _IDT VARCHAR(80) DEFAULT '-1',
     INDEX SERIAL,
     PRIMARY KEY (TABLE_SCHEMA, TABLE_NAME, _IDT)
@@ -32,6 +33,7 @@ func defineRecords(db *DB) error {
   CREATE INDEX IF NOT EXISTS RECORDS_TABLE_SCHEMA_IDX ON core.RECORDS(TABLE_SCHEMA);
   CREATE INDEX IF NOT EXISTS RECORDS_TABLE_NAME_IDX ON core.RECORDS(TABLE_NAME);
 	CREATE INDEX IF NOT EXISTS RECORDS_OPTION_IDX ON core.RECORDS(OPTION);
+	CREATE INDEX IF NOT EXISTS RECORDS_SYNC_IDX ON core.RECORDS(SYNC);
   CREATE INDEX IF NOT EXISTS RECORDS__IDT_IDX ON core.RECORDS(_IDT);  
 	CREATE INDEX IF NOT EXISTS RECORDS_INDEX_IDX ON core.RECORDS(INDEX);`
 
@@ -46,41 +48,77 @@ func defineRecords(db *DB) error {
 
 func defineRecordsFunction(db *DB) error {
 	sql := `
-	CREATE OR REPLACE FUNCTION core.RECORDS_BEFORE_INSERT()
+	CREATE OR REPLACE FUNCTION core.SYNC_NOTIFY()
   RETURNS
     TRIGGER AS $$  
   BEGIN
+    IF NEW.SYNC = FALSE THEN      
+			PERFORM pg_notify(
+			'sync',
+			json_build_object(
+				'schema', NEW.TABLE_SCHEMA,
+				'table', NEW.TABLE_NAME,
+				'option', NEW.OPTION,        
+				'_idt', NEW._IDT
+			)::text
+			);		
+		END IF;
+  RETURN NEW;
+  END;
+  $$ LANGUAGE plpgsql;
+
+	DROP TRIGGER IF EXISTS SYNC_AFTER_INSERT ON core.RECORDS CASCADE;
+	CREATE TRIGGER SYNC_AFTER_INSERT
+	AFTER INSERT ON $1
+	FOR EACH ROW
+	EXECUTE PROCEDURE core.SYNC_NOTIFY();
+
+	DROP TRIGGER IF EXISTS SYNC_AFTER_UPDATE ON core.RECORDS CASCADE;
+	CREATE TRIGGER SYNC_AFTER_UPDATE
+	AFTER UPDATE ON $1
+	FOR EACH ROW
+	EXECUTE PROCEDURE core.SYNC_NOTIFY();
+
+	CREATE OR REPLACE FUNCTION core.RECORDS_BEFORE_INSERT()
+  RETURNS
+    TRIGGER AS $$
+	DECLARE
+		VSYNC BOOLEAN;
+  BEGIN
     IF NEW._IDT = '-1' THEN
       NEW._IDT = uuid_generate_v4();
+			VSYNC = FALSE;
+		ELSE
+			VSYNC = TRUE;
 		END IF;
 
-		PERFORM pg_notify(
-		'before',
-		json_build_object(
-			'schema', TG_TABLE_SCHEMA,
-			'table', TG_TABLE_NAME,
-			'option', TG_OP,        
-			'_idt', NEW._IDT
-		)::text
-		);
-  RETURN NEW;
+		INSERT INTO core.RECORDS(TABLE_SCHEMA, TABLE_NAME, OPTION, SYNC, _IDT)
+		VALUES (TG_TABLE_SCHEMA, TG_TABLE_NAME, TG_OP, VSYNC, NEW._IDT);
+
+  	RETURN NEW;
   END;
   $$ LANGUAGE plpgsql;
 
 	CREATE OR REPLACE FUNCTION core.RECORDS_BEFORE_UPDATE()
   RETURNS
-    TRIGGER AS $$  
-  BEGIN    
-		PERFORM pg_notify(
-		'before',
-		json_build_object(
-			'schema', TG_TABLE_SCHEMA,
-			'table', TG_TABLE_NAME,
-			'option', TG_OP,
-			'_idt', NEW._IDT
-		)::text
-		);
-  RETURN NEW;
+    TRIGGER AS $$
+	DECLARE
+		VSYNC BOOLEAN;
+  BEGIN
+		IF OLD._IDT = NEW._IDT THEN
+			VSYNC = FALSE;
+		ELSE
+			NEW._IDT = OLD._IDT;
+			VSYNC = TRUE;
+		END IF;
+
+		UPDATE core.RECORDS SET
+		DATE_UPDATE=NOW(),
+		OPTION=TG_OP,
+		SYNC=VSYNC
+		WHERE _IDT=NEW._IDT;
+
+  	RETURN NEW;
   END;
   $$ LANGUAGE plpgsql;
 
@@ -88,86 +126,16 @@ func defineRecordsFunction(db *DB) error {
   RETURNS
     TRIGGER AS $$  
   BEGIN
-		PERFORM pg_notify(
-		'before',
-		json_build_object(
-			'schema', TG_TABLE_SCHEMA,
-			'table', TG_TABLE_NAME,
-			'option', TG_OP,
-			'_idt', OLD._IDT
-		)::text
-		);
-  RETURN OLD;
-  END;
-  $$ LANGUAGE plpgsql;
-	
-	CREATE OR REPLACE FUNCTION core.RECORDS_AFTER_INSERT()
-  RETURNS
-    TRIGGER AS $$  
-  BEGIN
-		INSERT INTO core.RECORDS(TABLE_SCHEMA, TABLE_NAME, OPTION, _IDT)
-		VALUES (TG_TABLE_SCHEMA, TG_TABLE_NAME, TG_OP, NEW._IDT);
-
-		PERFORM pg_notify(
-		'after',
-		json_build_object(
-			'schema', TG_TABLE_SCHEMA,
-			'table', TG_TABLE_NAME,
-			'option', TG_OP,        
-			'_idt', NEW._IDT
-		)::text
-		);
-  RETURN NEW;
-  END;
-  $$ LANGUAGE plpgsql;
-
-	CREATE OR REPLACE FUNCTION core.RECORDS_AFTER_UPDATE()
-  RETURNS
-    TRIGGER AS $$  
-  BEGIN
-    UPDATE core.RECORDS SET
+		UPDATE core.RECORDS SET
 		DATE_UPDATE=NOW(),
-		OPTION=TG_OP
-		WHERE _IDT=NEW._IDT;
-
-		PERFORM pg_notify(
-		'after',
-		json_build_object(
-			'schema', TG_TABLE_SCHEMA,
-			'table', TG_TABLE_NAME,
-			'option', TG_OP,
-			'_idt', NEW._IDT
-		)::text
-		);
-  RETURN NEW;
+		OPTION=TG_OP,
+		SYNC=FALSE
+		WHERE _IDT=OLD._IDT;
+		
+  	RETURN OLD;
   END;
   $$ LANGUAGE plpgsql;
-
-  CREATE OR REPLACE FUNCTION core.RECORDS_AFTER_DELETE()
-  RETURNS
-    TRIGGER AS $$  
-  BEGIN
-    DELETE FROM core.RECORDS
-    WHERE TABLE_SCHEMA = TG_TABLE_SCHEMA
-    AND TABLE_NAME = TG_TABLE_NAME
-    AND _IDT = OLD._IDT;
-
-		DELETE FROM core.COMMANDS
-    WHERE OPTION = 'UPDATE'
-    AND _ID = OLD._IDT;
-
-		PERFORM pg_notify(
-		'after',
-		json_build_object(
-			'schema', TG_TABLE_SCHEMA,
-			'table', TG_TABLE_NAME,
-			'option', TG_OP,
-			'_idt', OLD._IDT
-		)::text
-		);
-  RETURN OLD;
-  END;
-  $$ LANGUAGE plpgsql;`
+	`
 
 	id := "define-records-function"
 	_, err := db.Command(CommandDefine, id, sql)
---
+++ ws/handler.go
@@ -0,0 +1,312 @@
+package ws
+
+import (
+	"os"
+	"time"
+
+	"github.com/celsiainternet/elvis/envar"
+	"github.com/celsiainternet/elvis/et"
+	"github.com/celsiainternet/elvis/logs"
+)
+
+/**
+* Close
+**/
+func (h *Hub) Close() {
+	h.mutex.Lock()
+	defer h.mutex.Unlock()
+
+	logs.Log("Websocket", "Shutting down server...")
+
+	if !h.run {
+		return
+	}
+
+	h.run = false
+
+	close(h.register)
+	close(h.unregister)
+
+	for _, client := range h.clients {
+		client.close()
+	}
+	h.clients = nil
+
+	for _, channel := range h.channels {
+		channel.close()
+	}
+	h.channels = nil
+
+	if h.adapter != nil {
+		h.adapter.Close()
+	}
+}
+
+/**
+* Start
+**/
+func (h *Hub) Start() {
+	if h.run {
+		return
+	}
+
+	host, _ := os.Hostname()
+	h.Host = envar.GetStr(host, "WS_HOST")
+	h.run = true
+	logs.Logf("Websocket", "Run server on %s", h.Host)
+
+	for {
+		select {
+		case client := <-h.register:
+			h.onConnect(client)
+		case client := <-h.unregister:
+			h.onDisconnect(client)
+		}
+	}
+}
+
+/**
+* SetName
+* @param name string
+**/
+func (h *Hub) SetName(name string) {
+	h.Name = name
+}
+
+/**
+* Identify the hub
+* @return et.Json
+**/
+func (h *Hub) From() et.Json {
+	return et.Json{
+		"id":   h.Id,
+		"name": h.Name,
+	}
+}
+
+/**
+* GetChanel
+* @param name string
+* @return *Channel
+**/
+func (h *Hub) GetChanel(name string) *Channel {
+	idx := h.indexChannel(name)
+	if idx == -1 {
+		return nil
+	}
+
+	return h.channels[idx]
+}
+
+/**
+* NewChannel
+* @param name string
+* @param duration time.Duration
+* @return *Channel
+**/
+func (h *Hub) NewChannel(name string, duration time.Duration) *Channel {
+	idx := h.indexChannel(name)
+	if idx != -1 {
+		return h.channels[idx]
+	}
+
+	h.mutex.Lock() // Bloquear el mutex para evitar condiciones de carrera
+	defer h.mutex.Unlock()
+
+	result := newChannel(name)
+	h.channels = append(h.channels, result)
+
+	clean := func() {
+		result.close()
+	}
+
+	if duration > 0 {
+		go time.AfterFunc(duration, clean)
+	}
+
+	return result
+}
+
+/**
+* Subscribe
+* @param clientId string
+* @param channel string
+* @return error
+**/
+func (h *Hub) Subscribe(clientId string, channel string) error {
+	idxCl := h.indexClient(clientId)
+	if idxCl == -1 {
+		return logs.Alertm(ERR_CLIENT_NOT_FOUND)
+	}
+
+	client := h.clients[idxCl]
+	_channel := h.NewChannel(channel, 0)
+	_channel.subscribe(client, "")
+
+	return nil
+}
+
+/**
+* QueueSubscribe
+* @param clientId string
+* @param channel string
+* @param queue string
+* @return error
+**/
+func (h *Hub) QueueSubscribe(clientId string, channel, queue string) error {
+	idxCl := h.indexClient(clientId)
+	if idxCl == -1 {
+		return logs.Alertm(ERR_CLIENT_NOT_FOUND)
+	}
+
+	client := h.clients[idxCl]
+	_channel := h.NewChannel(channel, 0)
+	_channel.subscribe(client, queue)
+
+	return nil
+}
+
+/**
+* Stack
+* @param clientId string
+* @param channel string
+* @return error
+**/
+func (h *Hub) Stack(clientId string, channel string) error {
+	return h.QueueSubscribe(clientId, channel, "stack")
+}
+
+/**
+* Unsubscribe a client from hub channels
+* @param clientId string
+* @param channel string
+* @return error
+**/
+func (h *Hub) Unsubscribe(clientId string, channel string) error {
+	idxCh := h.indexChannel(channel)
+	if idxCh == -1 {
+		return nil
+	}
+
+	idxCl := h.indexClient(clientId)
+	if idxCl == -1 {
+		return nil
+	}
+
+	client := h.clients[idxCl]
+	_channel := h.channels[idxCh]
+	_channel.unsubscribe(client)
+
+	return nil
+}
+
+/**
+* Publish a message to a channel
+* @param channel string
+* @param msg Message
+* @param ignored []string
+* @param from et.Json
+* @return error
+**/
+func (h *Hub) Publish(channel string, msg Message, ignored []string, from et.Json) error {
+	idx := h.indexChannel(channel)
+	if idx == -1 {
+		return logs.NewErrorF(ERR_CHANNEL_NOT_FOUND, channel)
+	}
+
+	ch := h.channels[idx]
+	err := h.broadcast(ch, msg, ignored, from)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+/**
+* SendMessage
+* @param clientId string
+* @param msg Message
+* @return error
+**/
+func (h *Hub) SendMessage(clientId string, msg Message) error {
+	idx := h.indexClient(clientId)
+	if idx == -1 {
+		return logs.NewErrorF(ERR_CLIENT_NOT_FOUND, clientId)
+	}
+
+	if idx != -1 {
+		client := h.clients[idx]
+		return client.sendMessage(msg)
+	}
+
+	if h.adapter != nil {
+		h.adapter.Direct(clientId, msg)
+	}
+}
+
+/**
+* GetChannels of the hub
+* @param key string
+* @return et.Items
+**/
+func (h *Hub) GetChannels(key string) et.Items {
+	result := []et.Json{}
+	if key == "" {
+		for _, channel := range h.channels {
+			result = append(result, channel.describe())
+		}
+	} else {
+		idx := h.indexChannel(key)
+		if idx != -1 {
+			result = append(result, h.channels[idx].describe())
+		}
+	}
+
+	return et.Items{
+		Count:  len(result),
+		Ok:     len(result) > 0,
+		Result: result,
+	}
+}
+
+/**
+* GetClients of the hub
+* @param key string
+* @return et.Items
+**/
+func (h *Hub) GetClients(key string) et.Items {
+	result := []et.Json{}
+	if key == "" {
+		for _, client := range h.clients {
+			result = append(result, client.describe())
+		}
+	} else {
+		idx := h.indexClient(key)
+		if idx != -1 {
+			result = append(result, h.clients[idx].describe())
+		}
+	}
+
+	return et.Items{
+		Count:  len(result),
+		Ok:     len(result) > 0,
+		Result: result,
+	}
+}
+
+/**
+* DrainChannel
+* @param channel *Channel
+**/
+func (h *Hub) DrainChannel(channel string) error {
+	idx := h.indexChannel(channel)
+	if idx == -1 {
+		return logs.NewErrorF(ERR_CHANNEL_NOT_FOUND, channel)
+	}
+
+	ch := h.channels[idx]
+	ch.drain()
+
+	return nil
+}
