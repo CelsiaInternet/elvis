3839e1f9bb0573586d49bc73cd8d3f7a74056b47
Cesar Galvis Leon <cgalvisleon@gmail.com>
2024-11-08 07:49:49 -0500

    Backup

--- version.sh
+++ version.sh
@@ -6,11 +6,13 @@ MAYOR=false
 MINOR=false
 INDEX=2
 VERSION=$(git describe --tags --abbrev=0)               # Valor para reemplazar $VERSION obtenido de Git
-readme_file="README.md"                                 # Nombre del archivo a modificar
+README_FILE="README.md"                                 # Nombre del archivo a modificar
+HELP=true                                               # Bandera para mostrar la ayuda
 
 # Parsear opciones
 while [[ "$#" -gt 0 ]]; do
     case $1 in
+        --h | --help) HELP=true ;;                      # Mostrar la ayuda
         --m | --major) MAYOR=true ;;                    # Activar la bandera si se proporciona --major
         --n | --minor) MINOR=true ;;                    # Activar la bandera si se proporciona --minor
         --r | --rebuild) REBUILD=true ;;                # Activar la bandera si se proporciona --rebuild
@@ -30,6 +32,14 @@ latest_tag=$(git describe --tags --abbrev=0 2>/dev/null)
 # Si no hay etiquetas, usa la versión predeterminada v1.0.0
 if [ -z "$latest_tag" ]; then
   new_version="v1.0.0"
+elif [ "$HELP" == true ]; then
+  echo "Uso: ./version.sh [opciones]"
+  echo "Opciones:"
+  echo "  --h, --help     Mostrar esta ayuda"
+  echo "  --m, --major    Incrementar la versión mayor"
+  echo "  --n, --minor    Incrementar la versión menor"
+  echo "  --r, --rebuild  Incrementar el rebuild actual"
+  exit 0
 else
   # Divide la etiqueta en componentes usando el punto como delimitador
   IFS='.' read -r -a version_parts <<< "${latest_tag#v}"
@@ -56,7 +66,7 @@ else
   fi  
 fi
 
-sed -i '' "s/$VERSION/$new_version/g" "$readme_file"
+sed -i '' "s/$VERSION/$new_version/g" "$README_FILE"
 
 # Muestra la nueva versión
 echo "La nueva versión es: $new_version"
