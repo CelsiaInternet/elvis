798784241790e379614d3f8e9c641cd6b3ebee03
Cesar Galvis Leon <cgalvisleon@gmail.com>
2024-11-19 16:31:11 -0500

    Update

--- jrpc/msg.go
+++ jrpc/msg.go
@@ -4,5 +4,4 @@ const (
 	ERR_PACKAGE_NOT_FOUND = "RPC package not load"
 	ERR_SERVER_NOT_FOUND  = "RPC server not found"
 	ERR_METHOD_NOT_FOUND  = "Method not found - %s"
-	ERR_ENV_REQUIRED      = "Environment RPC_HOST required"
 )
