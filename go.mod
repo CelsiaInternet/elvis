94cf002a3f20704050fa8cc847dab48665ca0ebf
Cesar Galvis Leon <cgalvisleon@gmail.com>
2024-12-09 17:18:53 -0500

    Update

--- go.mod
+++ go.mod
@@ -5,15 +5,19 @@ go 1.22
 toolchain go1.23.0
 
 require (
+	github.com/bwmarrin/snowflake v0.3.0
+	github.com/cgalvisleon/et v0.0.8
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/golang-jwt/jwt/v4 v4.5.0
-	github.com/google/uuid v1.3.1
-	github.com/gorilla/websocket v1.5.0
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/websocket v1.5.1
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/manifoldco/promptui v0.9.0
+	github.com/matoous/go-nanoid/v2 v2.1.0
 	github.com/nats-io/nats.go v1.36.0
-	github.com/redis/go-redis/v9 v9.6.2
+	github.com/oklog/ulid v1.3.1
+	github.com/redis/go-redis/v9 v9.7.0
 	github.com/schollz/progressbar/v3 v3.14.6
 	golang.org/x/crypto v0.28.0
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56
@@ -26,7 +30,7 @@ require (
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/term v0.25.0 // indirect
 )
 
@@ -38,6 +42,6 @@ require (
 	github.com/nats-io/nkeys v0.4.7 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/sijms/go-ora/v2 v2.8.22
-	github.com/spf13/cobra v1.7.0
+	github.com/spf13/cobra v1.8.0
 	golang.org/x/sys v0.26.0 // indirect
 )
--- utility/utility.go
+++ utility/utility.go
@@ -14,7 +14,6 @@ import (
 	"github.com/celsiainternet/elvis/stdrout"
 	"github.com/celsiainternet/elvis/strs"
 	"github.com/celsiainternet/elvis/timezone"
-	"github.com/google/uuid"
 	"golang.org/x/exp/slices"
 )
 
@@ -73,23 +72,6 @@ func Now() string {
 * @param length int
 * @return string
 **/
-func GetOTP(length int) string {
-	const charset = "0123456789"
-	var seededRand = rand.New(rand.NewSource(timezone.NowTime().UnixNano()))
-
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
-	}
-
-	return string(b)
-}
-
-/**
-* GetOTP return a random number
-* @param length int
-* @return string
-**/
 func GetPassword(length int) string {
 	const charset = "0123456789"
 	var seededRand = rand.New(rand.NewSource(timezone.NowTime().UnixNano()))
@@ -100,48 +82,6 @@ func GetPassword(length int) string {
 	}
 
 	return string(b)
-}
-
-/**
-* UUID
-* @return string
-**/
-func UUID() string {
-	return uuid.NewString()
-}
-
-/**
-* NewId
-* @return string
-**/
-func NewId() string {
-	return UUID()
-}
-
-/**
-* GenId
-* @param id string
-* @return string
-**/
-func GenId(id string) string {
-	if map[string]bool{"": true, "*": true, "new": true}[id] {
-		return NewId()
-	}
-
-	return id
-}
-
-/**
-* GenKey
-* @param id string
-* @return string
-**/
-func GenKey(id string) string {
-	if map[string]bool{"": true, "-1": true, "*": true, "new": true}[id] {
-		return uuid.NewString()
-	}
-
-	return id
 }
 
 /**
