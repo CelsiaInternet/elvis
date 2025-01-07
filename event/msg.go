f50fba7d6d53792142ef825e95ba3253f0390f43
Cesar Galvis Leon <cgalvisleon@gmail.com>
2025-01-07 15:20:06 -0500

    Update

--- event/msg.go
+++ event/msg.go
@@ -1,10 +1,10 @@
 package event
 
 const (
-	ERR_ENV_REQUIRED     = "Variables de entorno requerida (%s)"
-	ERR_NOT_CONNECT      = "Not connect nats"
-	ERR_PARAM_NOT_FOUND  = "Param not found"
-	ERR_CLIENT_ID_EMPTY  = "Client id is empty"
+	ERR_ENV_REQUIRED     = "variables de entorno requerida (%s)"
+	ERR_NOT_CONNECT      = "not connect nats"
+	ERR_PARAM_NOT_FOUND  = "param not found"
+	ERR_CLIENT_ID_EMPTY  = "client id is empty"
+	ERR_CHANNEL_REQUIRED = "channel is required"
 	PARAMS_UPDATED       = "Params updated"
-	ERR_CHANNEL_REQUIRED = "Channel is required"
 )
