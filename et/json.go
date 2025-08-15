c28e1ef144ea4e207351b9dde0ea91cf164b5ae7
Cesar Galvis Leon <cgalvisleon@gmail.com>
2025-08-15 08:57:07 -0500

    Backup

--- et/json.go
+++ et/json.go
@@ -310,9 +310,11 @@ func (s Json) ValBool(_default bool, atribs ...string) bool {
 		case "false":
 			return false
 		default:
+			log.Println("ValBool value is not bool, type:", reflect.TypeOf(v), "value:", v)
 			return _default
 		}
 	default:
+		log.Println("ValBool value is not bool, type:", reflect.TypeOf(v), "value:", v)
 		return _default
 	}
 }
