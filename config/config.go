d085ab1778613ee979f8a1ea981b293c85b18ef6
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-09-16 13:06:50 -0500

    Backup:

--- config/config.go
+++ config/config.go
@@ -1,11 +1,6 @@
 package config
 
 import (
-	"slices"
-
-	"github.com/celsiainternet/elvis/envar"
-	"github.com/celsiainternet/elvis/et"
-	"github.com/celsiainternet/elvis/strs"
 	"github.com/celsiainternet/elvis/utility"
 )
 
@@ -14,43 +9,7 @@ import (
 * @param et.Json config
 * @return error
 **/
-func Load(config et.Json) error {
-	upsetVal := func(k string, v interface{}) {
-		switch val := v.(type) {
-		case string:
-			envar.UpSetStr(k, val)
-		case int:
-			envar.UpSetInt(k, val)
-		case bool:
-			envar.UpSetBool(k, val)
-		case float64:
-			envar.UpSetFloat(k, val)
-		}
-	}
-
-	upsetArray := func(v map[string]interface{}, isPassword bool) {
-		for kk, vv := range v {
-			kk = strs.Uppcase(kk)
-			if isPassword {
-				vv = PasswordUnhash(vv.(string))
-			}
-			upsetVal(kk, vv)
-		}
-	}
-
-	for k, v := range config {
-		k = strs.Uppcase(k)
-
-		switch val := v.(type) {
-		case map[string]interface{}:
-			upsetArray(val, slices.Contains([]string{"PASSWORD", "PASSWORDS", "PASS"}, k))
-		case et.Json:
-			upsetArray(val, slices.Contains([]string{"PASSWORD", "PASSWORDS", "PASS"}, k))
-		default:
-			upsetVal(k, val)
-		}
-	}
-
+func Load(stage, packageName string) error {
 	return nil
 }
 
